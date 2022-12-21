c39b5928fc72a72af8edc934ca80095f085f3919
Akshat Mittal <akshatmittal2506@gmail.com>
2022-12-21 20:11:14 +0530

    implemented helmet

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import "./home.scss";
@@ -6,62 +7,79 @@ import "./home.scss";
 const Home = () => {
 	const [username, setUsername] = useState("");
 	const navigate = useNavigate();
-	useEffect(() => {
-		const title = "GitHub Profile Search";
-		document.title = title;
-		const ogImage = document.createElement("meta");
-		ogImage.setAttribute("property", "og:image");
-		ogImage.setAttribute(
-			"content",
-			"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-		);
-		document.head.replaceChild(
-			ogImage,
-			document.querySelector('meta[property="og:image"]')
-		);
-		const ogTitle = document.createElement("meta");
-		ogTitle.setAttribute("property", "og:title");
-		ogTitle.setAttribute("content", title);
-		document.head.replaceChild(
-			ogTitle,
-			document.querySelector('meta[property="og:title"]')
-		);
-		const ogDescription = document.createElement("meta");
-		ogDescription.setAttribute("property", "og:description");
-		ogDescription.setAttribute(
-			"content",
-			"Enter a GitHub username to view their profile and repositories."
-		);
-		document.head.replaceChild(
-			ogDescription,
-			document.querySelector('meta[property="og:description"]')
-		);
-	}, []);
 
 	return (
-		<main className="home">
-			<div className="home-header">
-				<h1>GitHub Profile</h1>
-				<p>
-					Enter a GitHub username to view their profile and
-					repositories.
-				</p>
-			</div>
-			<div className="home-search">
-				<input
-					type="text"
-					placeholder="Enter a GitHub username"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
+		<>
+			<Helmet>
+				<title>GitHub Profile Search</title>
+				<meta property="og:title" content="GitHub Profile Search" />
+				<meta
+					property="og:image"
+					content="https://avatars.githubusercontent.com/u/1?v=4"
 				/>
-				<Button
-					text="Search"
-					onClick={() => {
-						if (username) navigate(`/profile/${username}`);
-					}}
+				<meta
+					property="og:description"
+					content="Enter a GitHub username to view their profile and repositories."
 				/>
-			</div>
-		</main>
+				<meta
+					property="og:url"
+					content="https://react-seo-61.netlify.app/"
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:locale" content="en_US" />
+				<meta property="og:site_name" content="GitHub Profile Search" />
+				<meta name="twitter:card" content="summary" />
+				<meta name="twitter:site" content="@akshatmittal61" />
+				<meta name="twitter:creator" content="@akshatmittal61" />
+				<meta name="twitter:title" content="GitHub Profile Search" />
+				<meta
+					name="twitter:description"
+					content="Enter a GitHub username to view their profile and repositories."
+				/>
+				<meta
+					name="twitter:image"
+					content="https://avatars.githubusercontent.com/u/0?v=4"
+				/>
+				<meta
+					name="twitter:image:alt"
+					content="GitHub Profile Search"
+				/>
+				<meta
+					name="description"
+					content="Enter a GitHub username to view their profile and repositories."
+				/>
+				<meta
+					name="keywords"
+					content="GitHub, Profile, Search, React, SEO"
+				/>
+				<meta name="author" content="Akshat Mittal" />
+				<meta name="robots" content="index, follow" />
+				<meta name="googlebot" content="index, follow" />
+			</Helmet>
+			<main className="home">
+				<div className="home-header">
+					<h1>GitHub Profile</h1>
+					<p>
+						Enter a GitHub username to view their profile and
+						repositories.
+					</p>
+				</div>
+				<div className="home-search">
+					<input
+						type="text"
+						placeholder="Enter a GitHub username"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<Button
+						text="Search"
+						onClick={() => {
+							if (username) navigate(`/profile/${username}`);
+						}}
+					/>
+				</div>
+			</main>
+		</>
 	);
 };
 
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import { fetchGitHubProfile } from "../../API/Profile";
 import MaterialIcons from "../../components/MaterialIcons";
@@ -15,107 +16,123 @@ const Profile = () => {
 		};
 		getProfile();
 	}, [username]);
-	useEffect(() => {
-		const title = `${
-			profile?.name ? profile.name : username
-		} | GitHub Profile Search`;
-		document.title = title;
-		const ogImage = document.createElement("meta");
-		ogImage.setAttribute("property", "og:image");
-		ogImage.setAttribute("content", profile?.avatar_url);
-		// document.head.appendChild(ogImage);
-		document.head.replaceChild(
-			ogImage,
-			document.head.querySelector('meta[property="og:image"]')
-		);
-		const ogTitle = document.createElement("meta");
-		ogTitle.setAttribute("property", "og:title");
-		ogTitle.setAttribute("content", title);
-		// document.head.appendChild(ogTitle);
-		document.head.replaceChild(
-			ogTitle,
-			document.head.querySelector('meta[property="og:title"]')
-		);
-		const ogDescription = document.createElement("meta");
-		ogDescription.setAttribute("property", "og:description");
-		ogDescription.setAttribute("content", profile?.bio);
-		// document.head.appendChild(ogDescription);
-		document.head.replaceChild(
-			ogDescription,
-			document.head.querySelector('meta[property="og:description"]')
-		);
-		console.log(ogImage, ogTitle, ogDescription);
-	}, [profile, username]);
 
 	return (
-		<main className="profile">
-			<div className="profile-header">
-				<div className="profile-header-avatar">
-					<img src={profile?.avatar_url} alt={profile?.name} />
-				</div>
-				<div className="profile-header-info">
-					<h1>{profile?.name}</h1>
-					<h2>{profile?.bio}</h2>
-					<div>
-						<a
-							href={profile?.html_url}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<MaterialIcons icon="link" />
-						</a>
-						<a
-							href={profile?.blog}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<MaterialIcons icon="language" />
-						</a>
-						<a
-							href={`mailto:${profile?.email}`}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<MaterialIcons icon="email" />
-						</a>
+		<>
+			<Helmet>
+				<title>{`${
+					profile?.name ? profile.name : username
+				} | GitHub Profile Search`}</title>
+				<meta property="og:image" content={profile?.avatar_url} />
+				<meta
+					property="og:title"
+					content={`${
+						profile?.name ? profile.name : username
+					} | GitHub Profile Search`}
+				/>
+				<meta property="og:description" content={profile?.bio} />
+				<meta property="og:url" content={window.location.href} />
+				<meta property="og:type" content="website" />
+				<meta property="og:site_name" content="GitHub Profile Search" />
+				<meta property="og:locale" content="en_US" />
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta
+					property="twitter:site"
+					content={`@${profile?.twitter_username}`}
+				/>
+				<meta
+					property="twitter:creator"
+					content={`@${profile?.twitter_username}`}
+				/>
+				<meta
+					property="twitter:title"
+					content={`${
+						profile?.name ? profile.name : username
+					} | GitHub Profile Search`}
+				/>
+				<meta property="twitter:description" content={profile?.bio} />
+				<meta property="twitter:image" content={profile?.avatar_url} />
+				<meta property="twitter:image:alt" content={profile?.name} />
+				<meta property="twitter:url" content={window.location.href} />
+				<meta name="description" content={profile?.bio} />
+				<meta name="keywords" content="GitHub, Profile, Search" />
+				<meta
+					name="author"
+					content={profile?.name ? profile.name : username}
+				/>
+			</Helmet>
+			<main className="profile">
+				<div className="profile-header">
+					<div className="profile-header-avatar">
+						<img src={profile?.avatar_url} alt={profile?.name} />
+					</div>
+					<div className="profile-header-info">
+						<h1>{profile?.name}</h1>
+						<h2>{profile?.bio}</h2>
+						<div>
+							<a
+								href={profile?.html_url}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<MaterialIcons icon="link" />
+							</a>
+							<a
+								href={profile?.blog}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<MaterialIcons icon="language" />
+							</a>
+							<a
+								href={`mailto:${profile?.email}`}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<MaterialIcons icon="email" />
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="profile-stats">
-				<div className="profile-stats-item">
-					<h1>{profile?.public_repos}</h1>
-					<h2>Repositories</h2>
+				<div className="profile-stats">
+					<div className="profile-stats-item">
+						<h1>{profile?.public_repos}</h1>
+						<h2>Repositories</h2>
+					</div>
+					<div className="profile-stats-item">
+						<h1>{profile?.followers}</h1>
+						<h2>Followers</h2>
+					</div>
+					<div className="profile-stats-item">
+						<h1>{profile?.following}</h1>
+						<h2>Following</h2>
+					</div>
 				</div>
-				<div className="profile-stats-item">
-					<h1>{profile?.followers}</h1>
-					<h2>Followers</h2>
-				</div>
-				<div className="profile-stats-item">
-					<h1>{profile?.following}</h1>
-					<h2>Following</h2>
-				</div>
-			</div>
-			<div className="profile-repos">
-				<h1>Repositories</h1>
-				<div className="profile-repos-list">
-					{profile?.repos?.map((repo) => (
-						<div className="profile-repos-list-item" key={repo.id}>
-							<h1>{repo.name}</h1>
-							<h2>{repo.description}</h2>
-							<div>
-								<a
-									href={repo?.html_url}
-									target="_blank"
-									rel="noreferrer"
-								>
-									<MaterialIcons icon="link" />
-								</a>
+				<div className="profile-repos">
+					<h1>Repositories</h1>
+					<div className="profile-repos-list">
+						{profile?.repos?.map((repo) => (
+							<div
+								className="profile-repos-list-item"
+								key={repo.id}
+							>
+								<h1>{repo.name}</h1>
+								<h2>{repo.description}</h2>
+								<div>
+									<a
+										href={repo?.html_url}
+										target="_blank"
+										rel="noreferrer"
+									>
+										<MaterialIcons icon="link" />
+									</a>
+								</div>
 							</div>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
-			</div>
-		</main>
+			</main>
+		</>
 	);
 };
 
